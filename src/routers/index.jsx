eb6d55084b42d418b05c8d89f082606d2bc63d09
Jon <joncaesar@gmail.com>
2023-02-25 21:14:45 +0800

    Update index.jsx

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Default from '../layouts/default.jsx';
-import Dashboard from '../views/Dashboard/index.jsx';
-import Users from '../views/Dashboard/users/users.jsx';
+import Dashboard from '../views/Dashboard/dashboard.jsx';
+import Users from '../views/Dashboard/employees/employees.jsx';
+import { Login } from '../views/Login/login.jsx';
 
 const IndexRouter = () => {
   return (
       <>
         <Routes>
+          <Route path='/' element={<Login />}/>
           <Route path='/admin' element={<Default />}>
+            <Route index path='/admin/' element={<Dashboard />} />
             <Route index path='/admin/dashboard' element={<Dashboard />} />
             <Route path='/admin/users' element={<Users />} />
           </Route> 
