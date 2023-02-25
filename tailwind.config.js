b06509e0901515c180fdcd1545815b21feb43f9d
Jon <joncaesar@gmail.com>
2023-02-25 21:15:19 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      whiteSmoke: '#F5F5F5',
+    }
   },
   plugins: [require('flowbite/plugin')],
 }
