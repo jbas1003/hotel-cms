a3c78c6d9527c0bd25131096db3361a4dc1807b6
Jon <joncaesar@gmail.com>
2023-02-26 13:49:11 +0800

    Update default.jsx

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -9,8 +9,8 @@ const Default = () => {
     <>
         <NavBar />
         <SideBar />
-        <div class="p-4 sm:ml-64">
-          <div class="p-4 mt-14">
+        <div className="p-4 sm:ml-64">
+          <div className="p-4 mt-14">
             <Outlet />
           </div>
         </div>
